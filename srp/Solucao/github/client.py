8a1b71fe5e38a1504cc0b41f8355c87a9379eddb
Anderson Pereira <andersononus@hotmail.com>
2020-10-18 23:30:42 -0300

    srp solucao e violacao

---
+++ srp/Solucao/github/client.py
@@ -0,0 +1,12 @@
+import requests
+
+class GithubClient():
+
+    API_BASE_URL = 'https://api.github.com'
+    @classmethod
+    def get_repos_by_user(self, user):
+        response = requests.get(f'{self.API_BASE_URL}/users/{user}/repos')
+        if response.status_code == 200:
+            return { "status_code": response.status_code, "body": response.json() }
+        else:
+            return {"status_code": response.status_code, "body": "Erro while getting repos"}
