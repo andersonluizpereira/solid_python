e4773db746a8cc42669e3592f5ef271dd57a2ce9
Anderson Pereira <andersononus@hotmail.com>
2020-10-23 19:18:57 -0300

    ocp add files

---
+++ main.py
@@ -0,0 +1,19 @@
+from ocp.Solucao.repos.reports.html_generator import HTMLGenerator
+from ocp.Solucao.repos.reports.markdown_generator import MarkdownGenerator
+from ocp.Solucao.repos.reports_generator import ReportsGenerator
+from srp.Solucao.github.client import GithubClient
+from srp.Solucao.repo.parser import RepoParser
+
+if __name__ == '__main__':
+    username = 'andersonluizpereira'
+    response = GithubClient.get_repos_by_user(username)
+
+    if response['status_code'] == 200:
+        repos = RepoParser.parse(response['body'])
+        markdown_report = ReportsGenerator.build(MarkdownGenerator, repos)
+        html_report = ReportsGenerator.build(HTMLGenerator, repos)
+
+        print(html_report)
+        print(markdown_report)
+    else:
+        print(response['body'])
